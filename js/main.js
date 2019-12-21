65d237389147b4242bd3261d288a3a38726fa545
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2019-12-21 09:27:41 -0500

    weather app initial.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
-const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q="
 
-const api_key = 'Your key here';
+const api_key = ''
+
+const cityWeatherInput = document.querySelector('#city')
+const cityWeatherInputButton = document.querySelector('#getTemp')
+const cityWeatherResults = document.querySelector('#results')
+
+cityWeatherInputButton.addEventListener('click', inputButtonClick)
+
+const inputButtonClick = event =>
+{
+    event.preventDefault()
+    let query = cityWeatherInput.value
+    collectResults(query)
+}
+
+const collectResults = query =>
+{
+    try
+    {
+        let results = await axios.get(`${weatherUrl}${query}${api_key}`)
+        console.log(results)
+        renderResults(results)
+    } catch (error)
+    {
+        console.log(`Oops! There was an error: ${error}`)
+    }
+}
+
+
+const renderResults = results =>
+{
+    console.log("Hello from function")
+}
+
+renderResults()
+
 
